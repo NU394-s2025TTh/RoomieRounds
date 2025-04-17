e79a239519a17d80282947481a3672bf1eedae4c
Anthony Behery <beheryanthony@gmail.com>
2025-04-16 23:20:21 -0500

    fixed bug with same day times

--- src/utils/getHumanReadableDay.ts
+++ src/utils/getHumanReadableDay.ts
@@ -1,7 +1,13 @@
 export function formatDueDate(day: string): string {
   const now = new Date();
   const due = new Date(day);
-  const diffInDays = Math.floor((due.getTime() - now.getTime()) / (1000 * 60 * 60 * 24));
+
+  const nowDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const dueDate = new Date(due.getFullYear(), due.getMonth(), due.getDate());
+
+  const diffInDays = Math.floor(
+    (dueDate.getTime() - nowDate.getTime()) / (1000 * 60 * 60 * 24),
+  );
   const timeString = due.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
 
   if (diffInDays === 0) {
